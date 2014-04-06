a4db4383e822d9df22b18f33a309fb8ce7943bc2
yattom <tsutomu.yasui@gmail.com>
2014-04-06 22:21:06 +0900

    refactoring Text and Paragraph
    Text and Paragraph stores only indexes of original data for reference

--- tests/text_test.py
+++ tests/text_test.py
@@ -1,7 +1,62 @@
+#coding: utf-8
+
 import unittest
+import tempfile
+from hamcrest import *
 
 from trans_server import text
 
+def assert_load_and_save(name, message=None):
+    t = load_data(name)
+    t.save()
+
+    testdata = TEXT_FOR_TESTS[name]
+    with open(t.path) as f:
+        d = f.read()
+        if type(testdata) is tuple and len(testdata) == 2:
+            assert_that(d, equal_to(testdata[0]), testdata[1])
+        else:
+            assert_that(d, equal_to(testdata[0]))
+
+
+def load_data(name):
+    testdata = TEXT_FOR_TESTS[name]
+    with tempfile.NamedTemporaryFile(delete=False) as f:
+        fname = f.name
+        f.write(testdata[0])
+
+    try:
+        return text.Text(fname)
+    finally:
+        f.unlink(f.name)
+
+TEXT_FOR_TESTS = {
+    'multi_para': (u'EnglishLine.\n日本語の行\n\nEnglishLine2.\n\n'.encode('utf-8'), 'multi paragraphs of single lines'),
+    'multi_line': (
+u'''
+English Line 1.
+English Line 2.
+English Line 3.
+日本語の行1。
+日本語の行2。
+日本語の行3。
+'''),
+}
+
+class TextLoadAndSaveTest(unittest.TestCase):
+    '''
+    Text must save contents exactly same as loaded.
+    '''
+    def test_multiparagraphs(self):
+        assert_load_and_save('multi_para')
+
+
 class TextTest(unittest.TestCase):
-    def test_fail(self):
-        self.fail()
+    def test_load_multi_para(self):
+        t = load_data('multi_para')
+        assert_that(len(t.paragraphs), is_(2))
+        assert_that(t.paragraphs[0].original_text(), is_('EnglishLine.\n'))
+        assert_that(t.paragraphs[0].translated_text(), is_(u'日本語の行\n'))
+        assert_that(t.paragraphs[1].original_text(), is_('EnglishLine2.\n'))
+        assert_that(t.paragraphs[1].translated_text(), is_(''))
+
--- trans_server/text.py
+++ trans_server/text.py
@@ -3,33 +3,71 @@ import re
 
 class Text(object):
     def __init__(self, path):
+        self.path = path
         self.paragraphs = []
         with open(path, encoding='utf8') as f:
-            paragraph = None
-            for l in f.readlines():
-                if not paragraph: paragraph = Paragraph()
-                l = l.strip()
-                if len(l) == 0:
-                    if paragraph:
-                        self.paragraphs.append(paragraph)
-                    paragraph = None
-                    continue
-                if re.search(u'[^ 0-9a-zA-Z.,?!:;/$()\u2014\u201c\u201d\u2019\u005b-]', l):
-                    paragraph.append_translated(l)
-                else:
-                    paragraph.append_original(l)
-            if paragraph:
-                self.paragraphs.append(paragraph)
+            self.data = f.read()
+
+        lines = []
+        head = 0
+        for i, c in enumerate(self.data):
+            if c == '\n':
+                lines.append((head, i + 1))
+                head = i + 1
+        if lines[-1][1] < len(self.data):
+            lines.append((head, len(self.data)))
+
+        paragraph = None
+        for (head, tail) in lines:
+            l = self.data[head:tail]
+            if not paragraph: paragraph = Paragraph(self)
+            l = l.strip()
+            if len(l) == 0:
+                if paragraph:
+                    self.paragraphs.append(paragraph)
+                paragraph = None
+                continue
+            if Text.is_translated(l):
+                paragraph.append_translated(head, tail)
+            else:
+                paragraph.append_original(head, tail)
+        if paragraph:
+            self.paragraphs.append(paragraph)
+
+    @staticmethod
+    def is_translated(line):
+        return re.search(u'[^ 0-9a-zA-Z.,?!:;/$()\u2014\u201c\u201d\u2019\u005b-]', line.strip())
+
+    def save(self):
+        with open(self.path, 'wb', encoding='utf8') as f:
+            f.write(self.data)
+
 
 class Paragraph(object):
-    def __init__(self):
-        self.original_lines = []
-        self.translated_lines = []
+    def __init__(self, text):
+        self.text = text
+        self.original = None
+        self.translated = None
         self.id = id(self)
 
-    def append_translated(self, line):
-        self.translated_lines.append(line)
+    def append_translated(self, head, tail):
+        if not self.translated:
+            self.translated = (head, tail)
+            return
+        assert head == self.translated[1]
+        self.translated = (self.translated[0], tail)
 
-    def append_original(self, line):
-        self.original_lines.append(line)
+    def append_original(self, head, tail):
+        if not self.original:
+            self.original = (head, tail)
+            return
+        assert head == self.original[1]
+        self.original = (self.original[0], tail)
 
+    def original_text(self):
+        if not self.original: return ''
+        return self.text.data[self.original[0]:self.original[1]]
+
+    def translated_text(self):
+        if not self.translated: return ''
+        return self.text.data[self.translated[0]:self.translated[1]]
