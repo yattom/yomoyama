6e1614e343ed03db3f1f92012d62b32532037def
yattom <tsutomu.yasui@gmail.com>
2014-04-06 09:02:36 +0900

    started writing tests for Text impl

---
+++ tests/text_test.py
@@ -0,0 +1,7 @@
+import unittest
+
+from trans_server import text
+
+class TextTest(unittest.TestCase):
+    def test_fail(self):
+        self.fail()
--- trans_server/__init__.py
+++ trans_server/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 app.config.from_object('trans_server.default_config')
-app.config.from_envvar('TRANS_SERVER_CONFIG')
+
+if 'TRANS_SERVER_CONFIG' in os.environ:
+    app.config.from_envvar('TRANS_SERVER_CONFIG')
 
 import trans_server.views
