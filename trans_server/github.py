2604f1e28bf2a7815fb92918bf9b716ca7205a36
yattom <tsutomu.yasui@gmail.com>
2014-04-13 02:56:22 -0400

    add appropriate scope to auth request

--- trans_server/github.py
+++ trans_server/github.py
@@ -1,4 +1,4 @@
-from flask import request, session, g, url_for, redirect
+from flask import request, session, g, url_for, redirect, json
 from flask import render_template_string
 from flask.ext.github import GitHub
 from trans_server import app
@@ -61,6 +61,7 @@ def authorized(access_token):
     user = User.query.filter_by(github_access_token=access_token).first()
     if user is None:
         user = User(access_token)
+        user.username = github.get('user', params={'access_token':access_token})[u'name']
         db_session.add(user)
     user.github_access_token = access_token
     db_session.commit()
@@ -71,7 +72,7 @@ def authorized(access_token):
 @app.route('/login')
 def login():
     if session.get('user_id', None) is None:
-        return github.authorize()
+        return github.authorize(scope='repo')
     else:
         return 'Already logged in'
 
