36f9ec26bb46ff47bd5a3af21759a5ec28ef930e
yattom <tsutomu.yasui@gmail.com>
2014-04-24 09:01:04 -0400

    config file for production on heroku

---
+++ production_config.py
@@ -0,0 +1,13 @@
+import os
+base_dir = os.path.dirname(__file__)
+
+# configuration for production on heroku
+
+BOOKS_DIR = base_dir + '/../data/books'
+DATABASE_URI = 'sqlite:///' + base_dir + '/../data/sqlite.db'
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# configuration for GitHub-Flask
+GITHUB_CLIENT_ID = os.environ['CLIENT_ID']
+GITHUB_CLIENT_SECRET = os.environ['CLIENT_SECRET']
+GITHUB_CALLBACK_URL = 'http://lit-depths-4578.herokuapp.com/github-callback'
