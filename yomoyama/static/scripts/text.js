c4e2bc5106b6936a7aea5ae2c39f0fb358583e34
yattom <tsutomu.yasui@gmail.com>
2014-05-03 23:04:16 -0400

    fixed a bug; saveing no longer causes multiple PUTs

--- yomoyama/static/scripts/text.js
+++ yomoyama/static/scripts/text.js
@@ -2,28 +2,25 @@ $(function() {
   $('div.editor').hide();
   $('div.editor textarea').flexible();
 
-  enable_edit_button = function() {
-    $('span.edit').click(function() {
-      pId = $(this).data('pId');
-      $('div[data-p-id=' + pId + '] .display').hide();
-      $('div[data-p-id=' + pId + '] .editor').show();
-      $('div[data-p-id=' + pId + '] .editor textarea').trigger('updateHeight');
-    });
+  do_edit = function() {
+    pId = $(this).data('pId');
+    $('div[data-p-id=' + pId + '] .display').hide();
+    $('div[data-p-id=' + pId + '] .editor').show();
+    $('div[data-p-id=' + pId + '] .editor textarea').trigger('updateHeight');
   };
-  enable_edit_button();
+  $('span.edit').click(do_edit);
 
-  enable_save_button = function() {
-    $('span.save').click(function() {
-      pId = $(this).data('pId');
+  do_save = function() {
+    pId = $(this).data('pId');
 
-      txt = $('div[data-p-id=' + pId + '] .editor textarea').val();
-      $.ajax({
-        url: $(location).attr('href') + '/paragraphs/' + pId,
-        type: 'PUT',
-        data: { text: txt },
-        success: function() {
-          $.get($(location).attr('href') + '/paragraphs/' + pId, function(data) {
-            $('div[data-p-id=' + pId + ']').html('\
+    txt = $('div[data-p-id=' + pId + '] .editor textarea').val();
+    $.ajax({
+      url: $(location).attr('href') + '/paragraphs/' + pId,
+      type: 'PUT',
+      data: { text: txt },
+      success: function() {
+        $.get($(location).attr('href') + '/paragraphs/' + pId, function(data) {
+          $('div[data-p-id=' + pId + ']').html('\
 <div class="en">' + data.original + '</div>\
 <div class="ja">\
   <div class="display">\
@@ -36,15 +33,15 @@ $(function() {
   </div>\
 </div>\
 ');
-            $('div[data-p-id=' + pId + '] .display').show();
-            $('div[data-p-id=' + pId + '] .editor').hide();
-            $('div[data-p-id=' + pId + '] .editor textarea').flexible();
-            enable_edit_button();
-            enable_save_button();
-          });
-        }
-      });
+          $('div[data-p-id=' + pId + '] .display').show();
+          $('div[data-p-id=' + pId + '] .editor').hide();
+          $('div[data-p-id=' + pId + '] .editor textarea').flexible();
+          $('div[data-p-id=' + pId + '] .editor span.edit').click(do_edit);
+          $('div[data-p-id=' + pId + '] .editor span.save').click(do_save);
+        });
+      }
     });
-  };
-  enable_save_button();
+  }
+ 
+  $('span.save').click(do_save);
 });
