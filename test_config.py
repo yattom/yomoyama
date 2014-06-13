f2b71b920c35df8cf72162f73c934f6e2befa1d3
yattom <tsutomu.yasui@gmail.com>
2014-06-13 08:59:42 -0400

    test run on testing env

---
+++ test_config.py
@@ -0,0 +1,13 @@
+import os
+
+# configuration for automated tests
+
+BOOKS_DIR = os.environ['BOOKS_DIR']
+DATABASE_URI = 'sqlite://' # in-memory for testing
+SECRET_KEY = 'TEST_SECRET_KEY'
+
+# configuration for GitHub-Flask
+# Don't use GitHub when testing (replace with mock)
+GITHUB_CLIENT_ID = 'NA'
+GITHUB_CLIENT_SECRET = 'NA'
+GITHUB_CALLBACK_URL = 'http://localhost:5001/github-callback'
