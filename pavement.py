0b7caf31f8e4fe4392d70cc4cf8d91b435509faf
yattom <tsutomu.yasui@gmail.com>
2014-06-13 05:39:04 -0400

    paver file to execute stuff

---
+++ pavement.py
@@ -0,0 +1,15 @@
+import sys
+import os
+from paver.easy import *
+import subprocess
+
+@task
+def test_end2end():
+    os.environ['PYTHONPATH']='.'
+    os.environ['YOMOYAMA_CONFIG']='../local_config.py'
+    os.environ['DISPLAY']=':1'
+    x=subprocess.Popen(['Xvfb', os.environ['DISPLAY']])
+    p=subprocess.Popen(['gunicorn','yomoyama.run:app'])
+    sh('./gradlew firefoxTest')
+    p.terminate()
+    x.terminate()
