a809a74a7a04d57d1c6c568b1886a97eae9db3a6
yattom <tsutomu.yasui@gmail.com>
2014-09-27 16:04:02 +0900

    refactoring tests

--- src/test/groovy/DeleteBookTest.groovy
+++ src/test/groovy/DeleteBookTest.groovy
@@ -18,14 +18,14 @@ class DeleteBookTest extends GebReportingTest {
         page.registerBook('DeleteMe', System.getenv('ORIGINAL_REPO_URL'), 'work_test')
 
         to TopPage
-        assert availableBooks[2].displayed
-        assert availableBooks[2].text() == 'DeleteMe'
+        assert bookByTitle('DeleteMe').displayed
+        int idx = bookIndexByTitle('DeleteMe')
         report('2')
 
-        deleteBooks[0].click()
+        deleteBooks[idx].click()
         to TopPage
         report('3')
-        assert !availableBooks[2].displayed
+        assert bookIndexByTitle('DeleteMe') == -1
     }
 }
 
--- src/test/groovy/GlossaryTest.groovy
+++ src/test/groovy/GlossaryTest.groovy
@@ -9,15 +9,15 @@ import org.junit.runners.JUnit4
 class GlossaryTest extends GebReportingTest {
 
     @Test
-    void anExample() {
+    void RegisterGlossaryEntryAndShow() {
         to TopPage
         newBookLink.click()
 
         at NewBookPage
-        page.registerBook('New Title', System.getenv('ORIGINAL_REPO_URL'), 'work_test')
+        page.registerBook('GlossaryTest1', System.getenv('ORIGINAL_REPO_URL'), 'work_test')
 
         to TopPage
-        availableBooks[0].click()
+        bookByTitle('GlossaryTest1').click()
         waitFor { at FilesPage }
 
         files[0].click()
@@ -31,23 +31,48 @@ class GlossaryTest extends GebReportingTest {
         glossary_register.click()
         report()
 
+        // TODO: just reload!
         to TopPage
         availableBooks[0].click()
         waitFor { at FilesPage }
         files[0].click()
         at TextPage
+
         assert glossary_for_paragraph(0).text().contains('English : 翻訳')
         report()
+    }
+
+    @Test
+    void MultipleWordsEntryForGlossary() {
+        to TopPage
+        newBookLink.click()
+
+        at NewBookPage
+        page.registerBook('GlossaryTest2', System.getenv('ORIGINAL_REPO_URL'), 'work_test')
+
+        to TopPage
+        bookByTitle('GlossaryTest2').click()
+        waitFor { at FilesPage }
+        files[0].click()
+        at TextPage
+        editAndSave(0, "翻訳文です。")
+
+        selectEnText(0, 0, 0)
+        assert glossary_en.text() == 'English'
+        selectJaText(0, 0, 1)
+        assert glossary_ja.text() == '翻訳'
+        glossary_register.click()
 
         selectEnText(0, 0, 1)
         assert glossary_en.text() == 'English Paragraph'
         selectJaText(0, 0, 2)
         assert glossary_ja.text() == '翻訳文'
         glossary_register.click()
+        report()
 
         // TODO: just reload!
         to TopPage
-        availableBooks[0].click()
+        bookByTitle('GlossaryTest2').click()
         waitFor { at FilesPage }
         files[0].click()
         at TextPage
